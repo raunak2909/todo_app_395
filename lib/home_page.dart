584ce7a464e26af608d867d9d57704eb0c6f3010
raunakpurohit <raunak.wscubetech@gmail.com>
2025-07-08 23:22:55 +0530

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app_395/db_helper.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> allTodo = [];
+  DBHelper? dbHelper;
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descController = TextEditingController();
+  int selectedPriority = 3;
+
+  @override
+  void initState() {
+    super.initState();
+    dbHelper = DBHelper.getInstance();
+    getAllTodo();
+  }
+
+  void getAllTodo() async {
+    allTodo = await dbHelper!.fetchAllTodo();
+    setState(() {
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('My Todos')),
+      body: allTodo.isNotEmpty
+          ? ListView.builder(
+        itemCount: allTodo.length,
+          itemBuilder: (_, index) {
+
+          Color bgColor = Colors.white;
+          int priority = allTodo[index][DBHelper.COLUMN_TODO_PRIORITY];
+
+          if(priority==1){
+            bgColor = Colors.blue.shade100;
+          } else if(priority==2){
+            bgColor = Colors.yellow.shade100;
+          } else if(priority==3){
+            bgColor = Colors.red.shade100;
+          }
+
+            return CheckboxListTile(
+              tileColor: bgColor,
+              value: allTodo[index][DBHelper.COLUMN_TODO_IS_COMPLETED] == 1,
+              onChanged: (value) async{
+                bool check = await dbHelper!.updateTaskCompletion(id: allTodo[index][DBHelper.COLUMN_TODO_ID], isCompleted: value ?? false);
+                if(check){
+                  getAllTodo();
+                }
+              },
+              title: Text(allTodo[index][DBHelper.COLUMN_TODO_TITLE], style: TextStyle(
+                decoration: allTodo[index][DBHelper.COLUMN_TODO_IS_COMPLETED]==1 ? TextDecoration.lineThrough : TextDecoration.none
+              ),),
+              subtitle: Text(allTodo[index][DBHelper.COLUMN_TODO_DESC], style: TextStyle(
+                  decoration: allTodo[index][DBHelper.COLUMN_TODO_IS_COMPLETED]==1 ? TextDecoration.lineThrough : TextDecoration.none
+              ),),
+            );
+      })
+          : Center(child: Text('No Todos')),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        showModalBottomSheet(context: context, builder: (_){
+          return Container(
+            width: double.infinity,
+            child: Column(
+              children: [
+                Text('Add Todo'),
+                TextField(
+                  controller: titleController,
+                ),
+                TextField(
+                  controller: descController,
+                ),
+                StatefulBuilder(
+                  builder: (context, ss) {
+                    return Row(
+                      children: [
+                        RadioMenuButton(value: 1, groupValue: selectedPriority, onChanged: (value){
+                          selectedPriority = value!;
+                          ss(() {
+
+                          });
+                        }, child: Text('Low')),
+                        RadioMenuButton(value: 2, groupValue: selectedPriority, onChanged: (value){
+                          selectedPriority = value!;
+                          ss(() {
+
+                          });
+                        }, child: Text('Med')),
+                        RadioMenuButton(value: 3, groupValue: selectedPriority, onChanged: (value){
+                          selectedPriority = value!;
+                          ss(() {
+
+                          });
+                        }, child: Text("High")),
+                      ],
+                    );
+                  }
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(onPressed: () async{
+                      bool check = await dbHelper!.addTodo(title: titleController.text, desc: descController.text, priority: selectedPriority);
+                      if(check){
+                        getAllTodo();
+                      }
+                      Navigator.pop(context);
+                    }, child: Text('Add')),
+                    ElevatedButton(onPressed: (){
+                      Navigator.pop(context);
+                    }, child: Text('Cancel')),
+                  ],
+                )
+              ],
+            ),
+          );
+        });
+      }, child: Icon(Icons.add),),
+    );
+  }
+}
